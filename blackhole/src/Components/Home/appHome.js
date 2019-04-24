f03f38e2302fab376343b5ac4c058f51261fc3b0
Eli Citron <ebcitron@gmail.com>
2019-04-24 09:04:05 -0700

    Organizing folders

---
+++ blackhole/src/Components/Home/appHome.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class appHome extends React.Component{
+
+    // put everything in here, this is what is gonna show when the login works, ie all components.. Ill do the nav bar
+    
+}
+
+export default appHome;
