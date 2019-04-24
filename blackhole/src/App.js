07db1ff0284057373803ab5f9f566772d270b029
theryanclausen <theryanclausen@gmail.com>
2019-04-24 08:48:47 -0700

    merged efforts

--- blackhole/src/App.js
+++ blackhole/src/App.js
@@ -1,30 +1,24 @@
-// import React from 'react';
-// import {connect } from 'react-redux';
+
+import React from 'react';
+import {withRouter} from 'react-router-dom';
+
+import './App.css';
+
+class App extends React.Component{
+  componentDidMount() {
+    console.log(window.location.pathname);
+    console.log(localStorage);
+  }
+
+  render(){
+  return (
+    <div className="App">
+      <Auth />
+    </div>
+  );
+}
+}
+const Auth = withRouter(Authenticate(HomeView)(LogRegView));
 
 
-// class App extends React.Component {
-// // CDM - check if user has token, if the token is in localstorage, login using this.props.login(JSON.parse(localStorage.getItem('username')));
-
-// //Display Nav bar with Username, hole/home, post, logout if logged in
-// //if logged out display nav bar with register, login, options.
-
-// //Render out the app, if user is logged in, display an internal App Home component with all of the logged in functionality,
-// //Else display the Registration/login page
-
-
-
-// const mapStateToProps = state => {
-// return {
-// username: this.state.login.username,
-// fetchingPosts: this.state.login.fetchingPosts,
-// postsFetched: this.state.login.postsFetched,
-// isLogingIn: this.state.login.isLogingIn,
-// isLoggedIn: this.state.login.isLoggedIn,
-
-
-// };
-
-
-// };
-
-// export default connect( mapStateToProps, {homepage, login, logout})(App);
+export default withRouter(App);
--- blackhole/src/index.js
+++ blackhole/src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import reducer from '.reducers';
-import thunk from 'redux-thunk';
-import {createStore, applyMiddleware} from 'redux';
-import {Provider } from 'react-redux';
-import logger from 'redux';
 
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
-<Provider store = {store}>
-<App />
-</Provider>, document.getElementById('root'));
